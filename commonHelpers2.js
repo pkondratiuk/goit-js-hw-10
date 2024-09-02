2cf17d763d27e4eb79755b3afc824beb769df4d8
Pavlo <95445682+pkondratiuk@users.noreply.github.com>
2024-09-02 10:18:48 +0300

    Initialize gh-pages

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/styles-971255c9.js";import{i}from"./assets/vendor-77e16229.js";const m=document.querySelector(".form");m.addEventListener("submit",c);function c(r){r.preventDefault();const t={};new FormData(r.target).forEach((e,o)=>{t[o]=e});const s=t.delay,a=t.state;new Promise((e,o)=>{setTimeout(()=>{a==="fulfilled"?e(s):o(s)},s)}).then(e=>{i.success({title:"Success",message:`✅ Fulfilled promise in ${e}ms`})}).catch(e=>{i.error({title:"Error",message:`❌ Rejected promise in ${e}ms`})})}
+//# sourceMappingURL=commonHelpers2.js.map
